48803b19e0f523021bb1504ad4b5bff24274c0e8
harshit_sharma <sharmaharshit769@gmail.com>
2024-02-19 19:44:43 +0530

    fixing backend

--- Router/routes.js
+++ Router/routes.js
@@ -4,27 +4,25 @@ const router = express.Router();
 const number_model = require('../db/Schema')
 
 router.post("/user_enter_number",async(req,res)=>{
+  // res.json({server : "bakend server running"})
     try {
-        const {ip }  = req.body
-        if(ip != null){
-          return;
-        }else{
-          const alreadystore = await number_model.find({ipadress : req.body.ipaddress})
-          
-          if(alreadystore){
-            return ;
-          }else {
-          console.log(req.body);
-        const newNumber = await number_model.create(req.body);
-        // console.log(req.body);
-        // console.log(req.body);
-        return res.json({status:true , msg : newNumber})
-          }
-        }
-        // res.status(201).json(newNumber);
-      } catch (err) {
-        res.status(400).json({ error: err.message });
-      }
+        const { ipaddress } = req.body;
+
+    // Check if IP address is provided
+    if (!ipaddress) {
+      return res.status(400).json({ error: "IP address is required" });
+    }
+    const existingEntry = await number_model.findOne({ ipaddress: ipaddress });
+    if (existingEntry) {
+      return res.status(400).json({ error: "IP address already exists in the database" });
+    }
+
+    // If IP address is provided and doesn't exist in the database, save the entry
+    const newNumber = await number_model.create(req.body);
+    return res.status(201).json({ status: true, msg: newNumber });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 })
 
 router.get("/user_enter_number_get",async(req,res)=>{
